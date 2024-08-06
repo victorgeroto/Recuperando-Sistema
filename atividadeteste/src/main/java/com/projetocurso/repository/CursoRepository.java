97f664030d931c17dca15b5274325396afbac21b
victorgeroto <132359789+victorgeroto@users.noreply.github.com>
2024-08-06 09:46:13 -0300

    Add files via upload

---
+++ atividadeteste/src/main/java/com/projetocurso/repository/CursoRepository.java
@@ -0,0 +1,11 @@
+package com.projetocurso.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.projetocurso.entities.Curso;
+
+
+public interface CursoRepository extends JpaRepository <Curso, Long>{
+
+}
